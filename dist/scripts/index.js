f0cf08ea6c84d39c322d28890cafad1f1b475ebf
Vlad Galcev <Vladgalcev03@gmail.com>
2022-08-17 22:28:51 +0300

    wip

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,2 +1,1 @@
 import{createPosts}from"./modules/blogs.js";import{loadPosts}from"./modules/data.js";import{pagination}from"./modules/pagination.js";const init=()=>{loadPosts(),pagination(),createPosts()};loadPosts(),pagination(),createPosts();
-//# sourceMappingURL=../maps/index.js.map
--- dist/scripts/modules/data.js
+++ dist/scripts/modules/data.js
@@ -1,2 +1,1 @@
 export const loadPosts=async t=>{let s;s=null===t?await fetch("https://gorest.co.in/public-api/posts"):await fetch(`https://gorest.co.in/public-api/posts?page=${t}`);return await s.json()};
-//# sourceMappingURL=../../maps/modules/data.js.map
